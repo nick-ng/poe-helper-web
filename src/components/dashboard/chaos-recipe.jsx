e88861261216536c34a1b4aca3a61de8f3ae478f
Nick Ng <nicholas.cj.ng@gmail.com>
2021-09-19 20:47:43 +1200

    added voice when chaos filter gets updated

--- src/components/dashboard/chaos-recipe.jsx
+++ src/components/dashboard/chaos-recipe.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 import { getSettings } from "../../utils";
+import { getVoices, sayWithVoice } from "../../services/text-to-speech";
+import { VOICE_CHARACTER_STORE, VOICE_VOLUME_STORE } from "../../constants";
 import { getStyle } from "./style";
 
 const defaultRegalAndChaos = [
@@ -46,35 +49,84 @@ const defaultRegalAndChaos = [
   },
 ];
 
+const Container = styled.div`
+  text-align: right;
+`;
+
+const VoiceControls = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
 export default function ({
   chaosItems = 0,
   lowestSlot = 0,
   regalAndChaos = defaultRegalAndChaos,
 }) {
-  const itemCountsString = JSON.stringify(
-    regalAndChaos.reduce((prev, curr) => {
-      prev[curr.slot] = curr.count;
-      return prev;
-    }, {})
+  const [prevItemTotals, setPrevItemTotals] = useState(0);
+  const [voices, setVoices] = useState([]);
+  const [selectedVoice, setSelectedVoice] = useState(
+    localStorage.getItem(VOICE_CHARACTER_STORE) || ""
   );
+  const [voiceVolume, setVoiceVolume] = useState(
+    parseFloat(localStorage.getItem(VOICE_VOLUME_STORE) || 0.3)
+  );
+
+  const itemTotals = regalAndChaos.reduce((prev, curr) => {
+    return prev + curr.count;
+  }, 0);
+
+  useEffect(() => {
+    const bb = async () => {
+      const newVoices = await getVoices();
+      setVoices(newVoices);
+      if (!selectedVoice) {
+        const defaultVoice = newVoices.filter((a) => a.default);
+        if (defaultVoice.length > 0) {
+          setSelectedVoice(defaultVoice[0].voiceURI);
+          localStorage.setItem(VOICE_CHARACTER_STORE, defaultVoice[0].voiceURI);
+        } else {
+          const enVoices = newVoices.filter((a) => a.lang.startsWith("en-"));
+          setSelectedVoice(enVoices[0].voiceURI);
+          localStorage.setItem(VOICE_CHARACTER_STORE, enVoices[0].voiceURI);
+        }
+      }
+    };
+
+    bb();
+  }, []);
 
   useEffect(() => {
     const { agentPort } = getSettings();
-    if (agentPort) {
-      const url = `http://localhost:${agentPort}/chaos-filter`;
-      fetch(url, {
+    if (agentPort && itemTotals !== prevItemTotals) {
+      const itemCountsString = JSON.stringify(
+        regalAndChaos.reduce((prev, curr) => {
+          prev[curr.slot] = curr.count;
+          return prev;
+        }, {})
+      );
+
+      fetch(`http://localhost:${agentPort}/chaos-filter`, {
         method: "POST",
         mode: "cors",
         headers: {
           "Content-Type": "application/json",
         },
         body: itemCountsString,
+      }).then(() => {
+        if (voiceVolume > 0) {
+          setPrevItemTotals(itemTotals);
+          sayWithVoice("Loot filter updated.", selectedVoice, {
+            volume: voiceVolume,
+          });
+        }
       });
     }
-  }, [itemCountsString]);
+  }, [itemTotals]);
 
   return (
-    <div>
+    <Container>
       <h2
         style={{
           color: chaosItems < lowestSlot ? "red" : "white",
@@ -99,6 +151,40 @@ export default function ({
             >{`${slot}: ${count} (${chaosCount})`}</div>
           ))}
       </div>
-    </div>
+      <VoiceControls>
+        <label>
+          Voice:&nbsp;
+          <select
+            onChange={(e) => {
+              localStorage.setItem(VOICE_CHARACTER_STORE, e.target.value);
+              setSelectedVoice(e.target.value);
+            }}
+            value={selectedVoice}
+          >
+            {voices
+              .filter((a) => a.lang.startsWith("en-"))
+              .map(({ name, voiceURI }) => (
+                <option key={voiceURI} value={voiceURI}>
+                  {name}
+                </option>
+              ))}
+          </select>
+        </label>
+        <label>
+          Volume:&nbsp;
+          <input
+            type="number"
+            value={voiceVolume}
+            min="0"
+            max="1"
+            step="0.05"
+            onChange={(e) => {
+              setVoiceVolume(parseFloat(e.target.value));
+              localStorage.setItem(VOICE_VOLUME_STORE, e.target.value);
+            }}
+          />
+        </label>
+      </VoiceControls>
+    </Container>
   );
 }
---
+++ src/services/text-to-speech.js
@@ -0,0 +1,92 @@
+const synth = window.speechSynthesis;
+let stopping = true;
+
+const sleep = (ms, output = null) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(output);
+    }, ms);
+  });
+
+export const getVoices = () => {
+  return new Promise((resolve, reject) => {
+    const bb = async () => {
+      for (let n = 0; n < 10000; n++) {
+        const voices = synth.getVoices();
+
+        if (voices.length > 0) {
+          resolve(voices);
+          return;
+        }
+
+        await sleep(10);
+      }
+      reject("Could not get voices");
+    };
+    if (speechSynthesis.hasOwnProperty("onvoiceschanged")) {
+      speechSynthesis.onvoiceschanged = bb;
+    } else {
+      bb();
+    }
+  });
+};
+
+const sayWithVoice0 = (phrase, voiceURI, { volume = 0.3, rate = 1 }) => {
+  if (volume <= 0) {
+    return;
+  }
+  const voice = synth.getVoices().find((voice) => voice.voiceURI === voiceURI);
+  const utterance = new SpeechSynthesisUtterance(phrase);
+  utterance.volume = volume;
+  utterance.rate = rate;
+  if (voice) {
+    utterance.voice = voice;
+    synth.speak(utterance);
+  }
+};
+
+export const sayWithVoice = async (
+  phrase,
+  voiceURI,
+  { volume = 0.3, rate = 1 }
+) => {
+  await getVoices();
+  sayWithVoice0(phrase, voiceURI, { volume, rate });
+};
+
+export const sayMultiplePhrases = async (
+  phrases,
+  voiceURI,
+  options,
+  callback
+) => {
+  const { start } = options;
+  stopping = false;
+  for (let i = start; i < phrases.length; i++) {
+    const phrase = phrases[i];
+    while (synth.speaking) {
+      await sleep(20);
+    }
+    if (stopping) {
+      return;
+    }
+    if (typeof callback === "function") {
+      callback(i);
+    }
+    sayWithVoice(phrase, voiceURI, options);
+    await sleep(20);
+  }
+};
+
+export const stopTalking = () => {
+  stopping = true;
+  synth.cancel();
+};
+
+export const pauseTalking = () => {
+  synth.pause();
+};
+
+export const resumeTalking = () => {
+  synth.resume();
+};
